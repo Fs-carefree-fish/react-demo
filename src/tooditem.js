4fd2c4a3428a30455a55322e20c6f9915fb0920e
张铮 <723136370@qq.com>
2019-09-02 20:11:42 +0800

    proptype

--- src/tooditem.js
+++ src/tooditem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
 
@@ -9,17 +10,35 @@ class TodoItem extends Component {
 
   handleClick() {
     const {delect, idx} = this.props
-   delect(idx)
+    delect(idx)
   }
 
+  //父组件render执行时 子组件render也会执行
+
+  //state props一旦改变，render会执行
+
   render() {
-    const {content} = this.props
-    return (
-      <li onClick={this.handleClick}>
-        {content}
-      </li>
-    )
+    // const {content, test} = this.props
+    // return (
+    //   <li onClick={this.handleClick}>
+    //    {test} - {content}
+    //   </li>
+    // )
+
+    return React.createElement('div', {}, 'item')
   }
+}
+
+TodoItem.propTypes = {
+  //test:PropTypes.isRequired,
+  //content: PropTypes.string,
+  content: PropTypes.oneOfType([PropTypes.string,PropTypes.number]),
+  delect: PropTypes.func,
+  idx: PropTypes.number
+}
+
+TodoItem.defaultProps = {//默认值
+  test: 'hello world!'
 }
 
 export default TodoItem
