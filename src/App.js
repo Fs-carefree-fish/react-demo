e47a5a13dbc7977afbe4d3ce0a6c87cf88797817
张铮 <723136370@qq.com>
2019-09-03 10:10:55 +0800

    动画

--- src/App.js
+++ src/App.js
@@ -1,96 +1,30 @@
 import React from 'react';
 import { Component, Fragment } from 'react'; //占位Fragment
 import './App.css'
-import ToodItem from './tooditem'
 
 class App extends Component {
 
   constructor(props) {
     super(props)//调用父类
-
     this.state = {
-      inputVal: 'hello',
-      list: []
+      show: true
     }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleDelect = this.handleDelect.bind(this)
-
+    this.handleToggole = this.handleToggole.bind(this)
   }
 
   render() {
     return (
       <Fragment>
-        <label htmlFor="insertArea">输入内容</label>{/**不能用for */}
-        <input
-          id="insertArea"
-          className='input'
-          value={this.state.inputVal}
-          onChange={this.handleInputChange.bind(this)}
-          />
-        <button onClick={() => { this.handleBtnClick() }}>提交</button>
-        <ul ref={(ul) => this.ul = ul}>
-          {
-            this.getToodoItem()
-          }
-        </ul>
+        <h1 className={this.state.show ? 'show' : 'hide'}>hello!!!</h1>
+        <button onClick={this.handleToggole}>按下</button>
       </Fragment>
     )
   }
 
-
-  handleInputChange(e) {
-    // this.setState({
-    //   inputVal: e.target.value
-    // })
-    const val = e.target.value
-    // const val = this.input.value
-    this.setState(() => ({
-      inputVal: val
-    }))
-  }
-
-  handleBtnClick(e) {
-    //this.setState是一个异步函数
-    this.setState((prevState) => ({
-      list: [...prevState.list, prevState.inputVal],
-      inputVal: ''
-    }), () => {
-      //console.log(this.ul.querySelectorAll('li').length)
+  handleToggole() {
+    this.setState({
+      show:!this.state.show
     })
-    // this.setState({
-    //   list: [...this.state.list, this.state.inputVal],
-    //   inputVal: ''
-    // })
-
-  }
-
-  handleDelect(index) {
-    // const list = [...this.state.list]//拷贝 不建议直接修改state
-    // list.splice(index, 1)
-    // this.setState({
-    //   list: list
-    // })
-
-    this.setState((prevState) => {
-      const list = [...prevState.list]
-      list.splice(index, 1)
-      return { list }
-    })
-  }
-
-  getToodoItem() {
-    return (
-      this.state.list.map((item, index) => {
-        return (
-          <ToodItem
-            key={index}
-            content={item} // 传内容
-            idx={index}//向子组件传值 下标
-            delect={this.handleDelect} //传方法
-          />
-        )
-      })
-    )
   }
 }
 
